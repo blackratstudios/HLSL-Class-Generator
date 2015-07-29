7f6300106a37b136aef361da4352df817da43913
esmith-blackrat <eric@blackratstudios.us>
2015-07-28 18:37:07 -0700

    Added ShaderModel

---
+++ src/BlackRat.Shaders/ShaderModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackRat.Shaders
+{
+    public enum ShaderModel
+    {
+        Unsupported,
+
+        /// <summary>
+        /// Shader Model 4.0
+        /// </summary>
+        SM4_0,
+
+        /// <summary>
+        /// Shader Model 4.1
+        /// </summary>
+        SM4_1,
+
+        /// <summary>
+        /// Shader Model 5.0
+        /// </summary>
+        SM5_0,
+
+        /// <summary>
+        /// Shader Model 5.1, introduced with DirectX 12 and 11.3
+        /// </summary>
+        SM5_1
+    }
+}
