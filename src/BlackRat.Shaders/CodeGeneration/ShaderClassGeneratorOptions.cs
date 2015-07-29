5d71a3cb24a7c09b5bdac799dbcc9037eab4f717
esmith-blackrat <eric@blackratstudios.us>
2015-07-29 14:51:05 -0700

    Moved console project to src/ where it belongs

---
+++ src/BlackRat.Shaders/CodeGeneration/ShaderClassGeneratorOptions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackRat.Shaders.CodeGeneration
+{
+    public class ShaderClassGeneratorOptions
+    {
+        public ShaderClassGeneratorOptions(string className)
+        {
+            ClassName = className;
+        }
+
+        public string BytecodeVariableName { get; set; }
+
+        public string ClassName { get; private set; }
+    }
+}
