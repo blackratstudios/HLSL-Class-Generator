d08a2fe9eb368f6c58c66c5b74e3cdb75d6edc13
esmith-blackrat <eric@blackratstudios.us>
2015-07-28 18:46:40 -0700

    Changed ShaderModel from an enum to a struct

---
+++ src/BlackRat.Shaders/ShaderKind.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackRat.Shaders
+{
+    public enum ShaderKind
+    {
+        ComputeShader,
+        DomainShader,
+        GeometryShader,
+        HullShader,
+        PixelShader,
+        VertexShader
+    }
+}
--- src/BlackRat.Shaders/ShaderModel.cs
+++ src/BlackRat.Shaders/ShaderModel.cs
@@ -6,28 +6,94 @@ using System.Threading.Tasks;
 
 namespace BlackRat.Shaders
 {
-    public enum ShaderModel
+    public struct ShaderModel : IComparable<ShaderModel>, IEquatable<ShaderModel>
     {
-        Unsupported,
+        private readonly int majorVersion;
+        private readonly int minorVersion;
 
-        /// <summary>
-        /// Shader Model 4.0
-        /// </summary>
-        SM4_0,
+        public ShaderModel(int majorVersion, int minorVersion)
+        {
+            this.majorVersion = majorVersion;
+            this.minorVersion = minorVersion;
+        }
 
-        /// <summary>
-        /// Shader Model 4.1
-        /// </summary>
-        SM4_1,
+        #region Properties
 
-        /// <summary>
-        /// Shader Model 5.0
-        /// </summary>
-        SM5_0,
+        public int MajorVersion { get { return majorVersion; } }
 
-        /// <summary>
-        /// Shader Model 5.1, introduced with DirectX 12 and 11.3
-        /// </summary>
-        SM5_1
+        public int MinorVersion { get { return minorVersion; } }
+
+        #endregion Properties
+
+        public int CompareTo(ShaderModel other)
+        {
+            if(MajorVersion < other.MajorVersion)
+            {
+                return -1;
+            }
+            else if (MajorVersion > other.MajorVersion)
+            {
+                return 1;
+            }
+            else
+            {
+                return MinorVersion - other.MinorVersion;
+            }
+        }
+
+        public bool Equals(ShaderModel other)
+        {
+            return MajorVersion == other.MajorVersion &&
+                MinorVersion == other.MinorVersion;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is ShaderModel ? Equals((ShaderModel)obj) : false;
+        }
+
+        public override int GetHashCode()
+        {
+            return MajorVersion ^ MinorVersion;
+        }
+
+        public override string ToString()
+        {
+            return $"{MajorVersion}_{MinorVersion}";
+        }
+
+        #region Operators
+
+        public static bool operator==(ShaderModel lhs, ShaderModel rhs)
+        {
+            return lhs.Equals(rhs);
+        }
+
+        public static bool operator!=(ShaderModel lhs, ShaderModel rhs)
+        {
+            return !lhs.Equals(rhs);
+        }
+
+        public static bool operator<(ShaderModel lhs, ShaderModel rhs)
+        {
+            return lhs.CompareTo(rhs) < 0;
+        }
+
+        public static bool operator <=(ShaderModel lhs, ShaderModel rhs)
+        {
+            return lhs.CompareTo(rhs) <= 0;
+        }
+
+        public static bool operator >(ShaderModel lhs, ShaderModel rhs)
+        {
+            return lhs.CompareTo(rhs) > 0;
+        }
+
+        public static bool operator >=(ShaderModel lhs, ShaderModel rhs)
+        {
+            return lhs.CompareTo(rhs) >= 0;
+        }
+
+        #endregion Operators
     }
 }
