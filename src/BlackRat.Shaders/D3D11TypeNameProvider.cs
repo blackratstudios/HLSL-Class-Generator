d4711510c67b2781cde62ef2181873d01fbf2ab3
esmith-blackrat <eric@blackratstudios.us>
2015-07-28 19:51:41 -0700

    Added type name provider

---
+++ src/BlackRat.Shaders/D3D11TypeNameProvider.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackRat.Shaders
+{
+    public class D3D11TypeNameProvider : ID3DTypeNameProvider
+    {
+        public string ConstantBufferType => D3D11Types.ConstantBufferType;
+
+        public string DeviceContextType => D3D11Types.DeviceContextType;
+
+        public string DeviceType => D3D11Types.DeviceType;
+
+        public string InputLayoutType => D3D11Types.InputLayoutType;
+
+        public string SamplerType => D3D11Types.SamplerType;
+
+        public string ShaderResourceViewType => D3D11Types.ShaderResourceViewType;
+    }
+}
---
+++ src/BlackRat.Shaders/ID3DTypeNameProvider.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackRat.Shaders
+{
+    /// <summary>
+    /// Provides names of DirectX types (e.g. ID3D11Buffer)
+    /// </summary>
+    /// <remarks>
+    /// Should be implemented to provide types for multiple versions of DirectX, if necessary
+    /// </remarks>
+    public interface ID3DTypeNameProvider
+    {
+        string ConstantBufferType { get; }
+
+        string DeviceType { get; }
+
+        string DeviceContextType { get; }
+
+        string InputLayoutType { get; }
+
+        string SamplerType { get; }
+
+        string ShaderResourceViewType { get; }
+    }
+}
